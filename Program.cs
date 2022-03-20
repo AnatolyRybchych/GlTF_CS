f5e8bd9e2b413d45f2cde9afc4aee55ccb8e74b5
= <anatolyrybchych@github.com>
2022-03-21 01:28:48 +0200

    refactor & translating into object model

--- Program.cs
+++ Program.cs
@@ -2,11 +2,18 @@
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
+using static OpenTK.Graphics.OpenGL.GL;
 
 using GLTF;
+using GLTF.Layout;
+using GLTF.Object;
 
 class Program
 {
+    public static ObjectGLTF? obj = null;
+    public static GameWindow Window;
     public const string robot = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/BrainStem/glTF/BrainStem.gltf";
     static void Main(string[] args)
     {
@@ -14,8 +21,34 @@ class Program
 
         string gltfjson = client.GetAsync(robot).Result.Content.ReadAsStringAsync().Result;
 
-        var gltf = JsonSerializer.Deserialize<GLTF.GLTF>(gltfjson);   
+        var gltf = JsonSerializer.Deserialize<LayoutGLTF>(gltfjson);   
+        if(gltf == null || !gltf.CheckRequirements()) Console.WriteLine("gltf file is corrupted");
+        else
+        {
+            obj = new ObjectGLTF(gltf, 1);
+        }
 
-        System.Console.WriteLine(gltf.CheckRequirements());
+        while(Window.IsExiting == false)
+        {
+            Window.ProcessEvents();
+            Render();
+        }
+    }
+
+    public static void Render()
+    {
+        ClearColor(1,0,0,1);
+        Clear(ClearBufferMask.ColorBufferBit);
+    }
+
+
+    static Program()
+    {
+        Window = new GameWindow(new GameWindowSettings(){
+
+        },
+        new NativeWindowSettings(){
+            Size = new OpenTK.Mathematics.Vector2i(800, 600),
+        });
     }
 }
